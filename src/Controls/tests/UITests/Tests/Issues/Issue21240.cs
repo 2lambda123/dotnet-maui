266e39d4a428f553b6d2794b0a4b186a90b8b3e7
Jakub Florkowski <kubaflo123@gmail.com>
2024-05-15 00:53:24 +0200

    Added a UI Test (#21240)

---
+++ src/Controls/tests/UITests/Tests/Issues/Issue21240.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests.Issues
+{
+	public class Issue21240 : _IssuesUITest
+	{
+		public override string Issue => "FlyoutPage IsGestureEnabled not working";
+
+		public Issue21240(TestDevice device) : base(device)
+		{
+		}
+
+		[Test]
+		public void FlyoutShouldNotBePresented()
+		{
+			this.IgnoreIfPlatforms(new TestDevice[] { TestDevice.Mac, TestDevice.Windows });
+
+			App.WaitForElement("label");
+			App.SwipeLeftToRight(1, 500);
+
+			// The test passes if a flyout is not present
+			VerifyScreenshot();
+		}
+	}
+}
