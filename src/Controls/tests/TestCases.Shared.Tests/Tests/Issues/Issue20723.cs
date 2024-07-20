d17ded2de9bf2bd8b856aec49f56a218d602f51e
Jakub Florkowski <kubaflo123@gmail.com>
2024-07-20 02:30:36 +0200

    Added a test category

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20723.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20723.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Maui.TestCases.Tests.Issues
 		}
 
 		[Test]
+		[Category(UITestCategories.ActivityIndicator)]
 		public async Task IndicatorShouldNotBeCoveredByHeader()
 		{
 			_ = App.WaitForElement("refreshView");
