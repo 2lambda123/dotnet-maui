9b4a5cf82e6658f9453e5b221edc9c3d3b4293eb
Piers Deseilligny <42496508+piersdeseilligny@users.noreply.github.com>
2024-08-17 11:41:29 +0100

    Remove commented code

--- src/Controls/src/Core/Platform/ModalNavigationManager/ModalNavigationManager.iOS.cs
+++ src/Controls/src/Core/Platform/ModalNavigationManager/ModalNavigationManager.iOS.cs
@@ -143,7 +143,6 @@ namespace Microsoft.Maui.Controls.Platform
 					//This is branched, because if the modal is a popover and can't display correctly for some reason, we want it to fail
 					if(wrapper.ModalPresentationStyle == UIKit.UIModalPresentationStyle.Popover){
 						if(wrapper.PopoverPresentationController != null && WindowViewController.View != null){
-								//wrapper.PopoverPresentationController.SourceRect = new CoreGraphics.CGRect(100,100,10,10);
 								await WindowViewController.PresentViewControllerAsync(wrapper, animated);
 								await Task.Delay(5);
 						}
