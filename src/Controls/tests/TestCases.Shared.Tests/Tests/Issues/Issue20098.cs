5a0e65aaaf525271f717da2f3892cc2cb88f9faa
Jakub Florkowski <kubaflo123@gmail.com>
2024-09-01 01:43:57 +0200

    Rebase & test updates

--- src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20098.cs
+++ src/Controls/tests/TestCases.Shared.Tests/Tests/Issues/Issue20098.cs
@@ -4,27 +4,27 @@ using NUnit.Framework.Legacy;
 using UITest.Appium;
 using UITest.Core;
 
-namespace Microsoft.Maui.TestCases.Tests.Issues
+namespace Microsoft.Maui.TestCases.Tests.Issues;
+
+public class Issue20098: _IssuesUITest
 {
-	public class Issue20098: _IssuesUITest
+	public Issue20098(TestDevice device) : base(device)
 	{
-		public Issue20098(TestDevice device) : base(device)
-		{
-		}
+	}
 
-		public override string Issue => "iOS-specific Slider.UpdateOnTab";
+	public override string Issue => "iOS-specific Slider.UpdateOnTab";
 
-		[Test]
-		public void UpdateOnTabShouldWork()
-		{
-			_ = App.WaitForElement("slider");
-			var pickerRect = App.FindElement("slider").GetRect();
-			var label = App.FindElement("label");
-			App.Click(pickerRect.X + pickerRect.Width / 2, pickerRect.Y);
-			var text = label.GetText();
+	[Test]
+	[Category(UITestCategories.Picker)]
+	public void UpdateOnTabShouldWork()
+	{
+		_ = App.WaitForElement("slider");
+		var pickerRect = App.FindElement("slider").GetRect();
+		var label = App.FindElement("label");
+		App.Click(pickerRect.X + pickerRect.Width / 2, pickerRect.Y);
+		var text = label.GetText();
 
-			ClassicAssert.AreNotEqual("0", text);
-		}
+		ClassicAssert.AreNotEqual("0", text);
 	}
 }
 #endif
